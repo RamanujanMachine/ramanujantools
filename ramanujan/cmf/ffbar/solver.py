d780a558bdefcfc8ef77eeb55667f443372ed59f
Rotem Kalisch <rotemkalisch@gmail.com>
2024-02-04 16:38:22 +0200

    Add documentation

--- ramanujan/cmf/ffbar/solver.py
+++ ramanujan/cmf/ffbar/solver.py
@@ -4,23 +4,28 @@ from sympy.abc import x, y
 from ramanujan.cmf.ffbar import linear_condition, quadratic_condition
 
 
-def polynomial_coefficients(poly):
-    return sp.Poly(poly, x, y).coeffs()
-
-
-def extract_variables(expressions):
-    variables = set()
-    for expression in expressions:
-        variables.update(expression.free_symbols)
-    variables -= {x, y}
-    return variables
+def polynomial_coefficients(poly, variables=(x, y)):
+    """
+    Returns the coefficients of all monomials of `poly` in `variables`.
+    By default assumes variables are $(x, y)$.
+    """
+    return sp.Poly(poly, *variables).coeffs()
 
 
 def solve(expressions):
+    """
+    Returns a list of solutions that solve `expressions`.
+
+    The equation system represented by `expressions` is [expr = 0 for expr in expressions].
+    """
     return sp.solve(expressions, dict=True, manual=True)
 
 
 def solve_ffbar(f, fbar):
+    """
+    Returns all pairs of subsets of the input $f(x, y), \bar{f}(x, y)$ such that
+    each one is a valid solution of the linear condition and the quadratic condition.
+    """
     equations = [
         *polynomial_coefficients(linear_condition(f, fbar)),
         *polynomial_coefficients(quadratic_condition(f, fbar)),
