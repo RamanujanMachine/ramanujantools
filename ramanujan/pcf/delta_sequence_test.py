37b97d07ef895f78fa4ae8ae8ef3870036dd8e38
Tomer Raz <you@example.com>
2024-04-19 18:10:03 +0300

    Fix delta sequence test

--- ramanujan/pcf/delta_sequence_test.py
+++ ramanujan/pcf/delta_sequence_test.py
@@ -8,20 +8,20 @@ from ramanujan.cmf import CMF, known_cmfs
 from mpmath import zeta
 
 
-def test_correct_values():
+def test_delta_sequence_correct_values():
     pcf = PCF(2*n+1, n**2)
     limit = 4/mp.pi
     depth = 300
     
     test_deltas = pcf.delta_sequence(depth, limit)
     true_deltas = []
-    for dep in range(1, depth):
+    for dep in range(1, depth + 1):
         true_deltas.append(pcf.delta(dep, limit))
 
     assert test_deltas == true_deltas
 
 
-def test_apery():
+def test_delta_sequence_apery():
     mp.mp.dps = 5000
     # Apery's PCF
     pcf = PCF(34 * n**3 + 51 * n**2 + 27 * n + 5, -(n**6))
@@ -32,7 +32,7 @@ def test_apery():
     assert deltas[-1] > 0.086 and deltas[-1] < 0.87
 
 
-def test_no_limit():
+def test_delta_sequence_no_limit():
     mp.mp.dps = 10000
     pcf = PCF(6, (2*n+1)**2)
     limit = mp.pi + 3
