8f183ccd6ce7f3712df553b69bafacf581541cba
Rotem Kalisch <rotemkalisch@gmail.com>
2024-05-27 19:34:19 +0300

    Allow limit to select p, q in the most generic way.
    We should make it more specific when understand how these NxN limits work

--- ramanujan/limit.py
+++ ramanujan/limit.py
@@ -49,54 +49,73 @@ class Limit(Matrix):
         other_p, other_q = other.as_rational()
         return p * other_q == q * other_p
 
-    def precision(self, base: int = 10) -> int:
+    def precision(self, p_indices=[0, -1], q_indices=[1, -1], base: int = 10) -> int:
         """
         Returns the error in 'digits' for the PCF convergence.
 
         Args:
             base: The numerical base in which to return the precision (by default 10)
         """
-        diff = abs(mp.mpq(*(self.as_rational(-2))) - mp.mpq(*(self.as_rational(-1))))
+        diff = abs(
+            mp.mpq(*(self.as_rational(p_indices, q_indices)))
+            - mp.mpq(
+                *(
+                    self.as_rational(
+                        [p_indices[0], p_indices[1] - 1],
+                        [q_indices[0], q_indices[1] - 1],
+                    )
+                )
+            )
+        )
         return int(mp.floor(-mp.log(diff, base)))
 
-    def increase_precision(self) -> int:
+    def increase_precision(self, p_indices=[0, -1], q_indices=[1, -1]) -> int:
         """
         Increases the global mpmath precision to the lever required to handle this limit.
         Returns the current precision after the increase.
         """
-        requested_precision = self.precision() * 1.1  # Taking 10% digits buffer
+        requested_precision = (
+            self.precision(p_indices, q_indices) * 1.1
+        )  # Taking 10% digits buffer
         mp.dps = max(mp.dps, requested_precision)
         return mp.dps
 
-    def as_rational(self, col=-1) -> List:
+    def as_rational(self, p_indices=[0, -1], q_indices=[1, -1]) -> List:
         r"""
-        Returns the limit as a rational number as a list [p, q],
-        which is defined as the 2 upper values of column `col` of the matrix, the last column by default.
+        Returns the limit as a rational number $\frac{p}{q}$.
 
         Researcher's note: rational representation of the limit is so far only well-defined for 2x2 matrices,
         and we are still looking for a generalization of this representation for NxN matrices.
+        Args:
+            p_indices: the indices of the numerator $p$
+            q_indices: the indices of the denominator $q$
+        Returns:
+            a list of the form [p, q], representing the rational number.
         """
-        return list(self.col(col))[0:2]
+        return [self[*p_indices], self[*q_indices]]
 
-    def as_float(self) -> mp.mpf:
+    def as_float(self, p_indices=[0, -1], q_indices=[1, -1]) -> mp.mpf:
         r"""
         Returns the limit as a floating point number f, such that $m \cdot v = f$, where `m=self` and `v=vector`.
 
         This function increases the global mpmath precision if needed.
         """
-        self.increase_precision()
-        p, q = self.as_rational()
+        self.increase_precision(p_indices, q_indices)
+        p, q = self.as_rational(p_indices, q_indices)
         return mp.mpf(p) / mp.mpf(q)
 
-    def as_rounded_number(self) -> str:
+    def as_rounded_number(self, p_indices=[0, -1], q_indices=[1, -1]) -> str:
         """
         Same as `as_float`, but also rounds the result to the shortest number possible within the error range.
 
         This function increases the global mpmath precision if needed.
         """
-        return most_round_in_range(self.as_float(), 10 ** -self.precision())
+        return most_round_in_range(
+            self.as_float(p_indices, q_indices),
+            10 ** -self.precision(p_indices, q_indices),
+        )
 
-    def delta(self, L: mp.mpf) -> mp.mpf:
+    def delta(self, L: mp.mpf, p_indices=[0, -1], q_indices=[1, -1]) -> mp.mpf:
         r"""
         Calculates the irrationality measure $\delta$ defined, as:
         $|\frac{p_n}{q_n} - L| = \frac{1}{q_n}^{1+\delta}$
@@ -107,8 +126,8 @@ class Limit(Matrix):
         Returns:
             the delta value as defined above.
         """
-        self.increase_precision()
-        p, q = self.as_rational()
+        self.increase_precision(p_indices, q_indices)
+        p, q = self.as_rational(p_indices, q_indices)
         gcd = math.gcd(p, q)
         reduced_q = mp.fabs(q // gcd)
         return -(1 + mp.log(mp.fabs(L - (p / q)), reduced_q))
