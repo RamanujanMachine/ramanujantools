b9ae3a95616709f86c7a7bd9a2595b9acb6f5c52
Rotem Kalisch <rotemkalisch@gmail.com>
2024-11-11 09:24:25 +0200

    Fix import order

--- ramanujantools/__init__.py
+++ ramanujantools/__init__.py
@@ -2,8 +2,8 @@ from .integer_relation import IntegerRelation
 from .matrix import Matrix
 from .poly_matrix import PolyMatrix
 from .limit import Limit
-from .linear_recurrence import LinearRecurrence
 from .generic_polynomial import GenericPolynomial
+from .linear_recurrence import LinearRecurrence
 from .simplify_object import simplify
 
 from . import pcf
@@ -14,8 +14,8 @@ __all__ = [
     "Matrix",
     "PolyMatrix",
     "Limit",
-    "LinearRecurrence",
     "GenericPolynomial",
+    "LinearRecurrence",
     "simplify",
     "pcf",
     "cmf",
