ef532f277a2b6302c32f13ec7612b049e1d0f909
Rotem Kalisch <rotemkalisch@gmail.com>
2025-03-04 14:44:24 +0200

    Implement NumericMatrix

--- ramanujantools/flint_core/__init__.py
+++ ramanujantools/flint_core/__init__.py
@@ -1,5 +1,13 @@
 from .context import mpoly_ctx, FlintPoly, FlintContext
 from .rational import FlintRational
 from .matrix import FlintMatrix
+from .numeric_matrix import NumericMatrix
 
-__all__ = ["FlintRational", "FlintMatrix", "mpoly_ctx", "FlintPoly", "FlintContext"]
+__all__ = [
+    "FlintRational",
+    "FlintMatrix",
+    "mpoly_ctx",
+    "FlintPoly",
+    "FlintContext",
+    "NumericMatrix",
+]
