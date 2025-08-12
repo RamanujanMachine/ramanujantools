c70317804ec66ec4604f18735e600d90499bf068
Rotem Kalisch <rotemkalisch@gmail.com>
2025-08-12 17:26:35 +0300

    Change function order

--- ramanujantools/cmf/cmf.py
+++ ramanujantools/cmf/cmf.py
@@ -178,30 +178,6 @@ class CMF:
         random_matrix = list(self.matrices.values())[0]
         return random_matrix.rows
 
-    def dual(self) -> CMF:
-        """
-        Returns the dual CMF which is defined with inverse-transpose matrices.
-        """
-        return CMF(
-            {axis: self.M(axis).inverse().transpose() for axis in self.axes()},
-            validate=False,
-        )
-
-    def coboundary(self, coboundary: Matrix) -> CMF:
-        """
-        Returns the CMF up to a coboundary matrix U.
-        """
-
-        matrices = {
-            axis: matrix.coboundary(coboundary, axis, sign=True)
-            for axis, matrix in self.matrices.items()
-        }
-        negative_matrices = {
-            axis: matrix.coboundary(coboundary, axis, sign=False)
-            for axis, matrix in self._negative_matrices_cache.items()
-        }
-        return CMF(matrices, negative_matrices, validate=False)
-
     def _validate_axes_substitutions(self, substitutions: Position) -> None:
         if (
             axes_substitutions := self.axes().intersection(substitutions.keys())
@@ -242,6 +218,30 @@ class CMF:
             },
             validate=False,
         )
+
+    def dual(self) -> CMF:
+        """
+        Returns the dual CMF which is defined with inverse-transpose matrices.
+        """
+        return CMF(
+            {axis: self.M(axis).inverse().transpose() for axis in self.axes()},
+            validate=False,
+        )
+
+    def coboundary(self, coboundary: Matrix) -> CMF:
+        """
+        Returns the CMF up to a coboundary matrix U.
+        """
+
+        matrices = {
+            axis: matrix.coboundary(coboundary, axis, sign=True)
+            for axis, matrix in self.matrices.items()
+        }
+        negative_matrices = {
+            axis: matrix.coboundary(coboundary, axis, sign=False)
+            for axis, matrix in self._negative_matrices_cache.items()
+        }
+        return CMF(matrices, negative_matrices, validate=False)
 
     @staticmethod
     def walk_symbol() -> sp.Symbol:
