f5f8843eaf5ea299cbec9569a98171fc6f5426a4
Rotem Kalisch <rotemkalisch@gmail.com>
2026-07-31 17:02:33 +0300

    Add FLINT-backed SymbolicMatrix inverse

--- ramanujantools/flint_core/symbolic_matrix_test.py
+++ ramanujantools/flint_core/symbolic_matrix_test.py
@@ -1,4 +1,5 @@
 import sympy as sp
+import pytest
 from sympy.abc import n
 
 from ramanujantools import Matrix
@@ -35,6 +36,28 @@ def test_mul_scalar():
 
     assert flintify(m * 17) == flintify(m) * 17
     assert flintify(m * 17) == 17 * flintify(m)
+
+
+def test_inverse():
+    matrix = Matrix([[n + 1, n], [1, 2 * n + 3]])
+    symbolic = flintify(matrix)
+
+    assert symbolic.inverse().to_rt(factor=True) == matrix.inverse()
+    assert (symbolic * symbolic.inverse()).to_rt(factor=True) == Matrix.eye(2)
+
+
+def test_inverse_rational_entries():
+    matrix = Matrix([[1 / (n + 1), n], [1, 2]])
+    symbolic = flintify(matrix)
+
+    assert symbolic.inverse().to_rt(factor=True) == matrix.inverse()
+
+
+def test_inverse_singular():
+    symbolic = flintify(Matrix([[1, n], [n, n**2]]))
+
+    with pytest.raises(ZeroDivisionError, match="Singular polynomial matrix"):
+        symbolic.inverse()
 
 
 def test_walk():
